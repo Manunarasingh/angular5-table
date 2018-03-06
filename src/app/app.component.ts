c880ebaf86788584634c719ab81285e13d11665b
Manu Narasinge Gowda <mgowda@infinera.com>
2018-03-06 16:18:11 +0530

    datatable without using existing third party component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component , OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataService } from './service/service';
 
 export interface TableData {
@@ -13,23 +13,42 @@ export interface TableData {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService) { }
   columns: any[] = [
-    {field: 'id', header: 'SL No'},
-    {field: 'body', header: 'Content'},
-    {field: 'title', header: 'Title'}
+    { field: 'id', header: 'SL No' },
+    { field: 'body', header: 'Content' },
+    { field: 'title', header: 'Title' }
   ];
-  data: TableData[];
-  slFilter: number;
-  timeout: any;
-  selectedValue: TableData;
-  content = 'Primng Table';
+  data: any[];
+  content = 'Data Table';
+  sortOrder = 1;
   ngOnInit() {
     this.dataService.getTableData().then((data) => {
-        this.data = data;
+      this.data = data;
     });
   }
-  onPSliderChange(event, dt) {
-    dt.filter(event.value, 'id', 'gt');
+  getSortOrder(column) {
+    this.data.sort((a, b) => this.compareField(a, b, column) * this.sortOrder);
+    if (this.sortOrder === 1) {
+      this.sortOrder = -1;
+    } else {
+      this.sortOrder = 1;
+    }
+  }
+  compareField(rowA: any, rowB: any, field: string) {
+    const fieldB = false;
+    if (rowA[field] == null) {
+      return -1;
+    }
+    if (typeof rowA[field] === 'string') {
+      return rowA[field].localeCompare(rowB[field]);
+    }
+    if (typeof rowA[field] === 'number') {
+      if (rowA[field] > rowB[field]) {
+        return -1;
+      } else {
+        return 1;
+      }
+    }
   }
 }
